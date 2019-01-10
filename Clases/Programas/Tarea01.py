37057b8f072ee8e40e49d1d35063cb422ca83b14
409085596 <gladius-angeli@ciencias.unam.mx>
2019-01-10 22:56:26 +0000

    Primera tarea de Python

---
+++ Clases/Programas/Tarea01.py
@@ -0,0 +1,13 @@
+#Elías Jiménez Cruz 409085596
+#Tarea 01. Se usará el teorema de Pitágoras para calcular la medida de la hipotenusa con las medidas de los dos
+#catetos. El teorema dice que el cuadrado de la hipotenusa, en un triángulo rectángulo, es igual a la suma de los
+#cuadrados de los catetos. Es decir, si a y b son los catetos y c la hipotenusa, c**2 = a**2 + b**2. Sean a = 3 y b = 4
+#los catetos de un triángulo rectangulo dado. Se calculará la medida de la hipotenusa.
+
+from math import *
+
+a = 3
+b = 4
+c = sqrt(a**2 + b**2)
+
+print(c)
