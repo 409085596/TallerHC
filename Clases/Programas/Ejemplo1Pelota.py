4eeb682a3c9d45e2776c8b355a14cffa8575b2b5
409085596 <gladius-angeli@ciencias.unam.mx>
2019-01-10 22:22:11 +0000

    Primer programa en python 2

---
+++ Clases/Programas/Ejemplo1Pelota.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Elías Jiménez Cruz
+print 34*3 - 1/2 * 9.81 * 3**2 #Calcula la división entera
+print 34*3 - 1.0/2 * 9.81 * 3**2 #Calcula la división flotante
+print 34*1 - 1.0/2 * 9.81 * 1**2
+print 34*1.5 - 1.0/2 * 9.81 * 1.5**2
+print 34*5 - 1.0/2 * 9.81 * 5**2
+v0 = 34
+g = 9.81
+t = 5
+y = v0*t - 1.0/2*g*t**2
+print y
