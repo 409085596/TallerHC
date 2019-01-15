bdff1ad7663901ebe3319ea8bb5f11d104e8d8e0
409085596 <gladius-angeli@ciencias.unam.mx>
2019-01-15 14:03:36 -0600

    Programa raiz1.py

--- Clases/Programas/funcion01.py
+++ Clases/Programas/funcion01.py
@@ -3,3 +3,23 @@ def vAbsoluto(x):
         return(x)
     else:
         return(-x)
+
+def raiz(x):
+    h=x
+    b=1.0
+    e=0.0001
+    while vAbsoluto(b-h)>e:
+        h = (b+h)/2
+        b = x/h
+    return(b)
+
+def raiz1(x):
+    h=x
+    b=1.0
+    e=0.0001
+    contador=0
+    while vAbsoluto(b-h)>e:
+        h = (b+h)/2
+        b = x/h
+        contador = contador+1
+    return("El proceso se repitio %d veces" %(contador), "El resultado es %f" %(b))
