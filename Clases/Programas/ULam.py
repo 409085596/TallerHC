b742ee529cbdfcac6bef5d35d05fcd04a2bb56af
409085596 <gladius-angeli@ciencias.unam.mx>
2019-01-15 15:05:39 -0600

    Actualización del programa ULam.py

--- Clases/Programas/ULam.py
+++ Clases/Programas/ULam.py
@@ -1,9 +1,9 @@
 def updown(x):
     i = 0
-    while x > 1:
+    while x != 1:
         if x%2 == 0:
             x=x/2
         else:
             x=3*x+1
         i=i+1
-    return(i)
+    return("Las veces que el proceso se repitio para que el numero se redujera a 1 fueron: %d" %(i))
